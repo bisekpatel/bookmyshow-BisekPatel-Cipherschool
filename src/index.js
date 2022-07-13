ba8e2c7d89fb9c43b14c0d644a2e62339611347b
Bisek Patel <bisekpatel7@gmail.com>
2022-07-13 15:50:41 +0530

    Cipherschool Project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './stylesheets/index.css';
+import { HallForm } from './components/HallForm'
+
+ReactDOM.render(
+  <HallForm />,
+  document.getElementById('root')
+);
